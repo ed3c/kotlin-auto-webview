73b969e2eb0bdc8d2122629e48c723490fba0706
ed3c <mcnum01@gmail.com>
2026-08-15 13:11:33 +0800

    fix: use current Compose Wasm viewport API

--- composeApp/src/wasmJsMain/kotlin/dev/ed3c/autowebview/main.kt
+++ composeApp/src/wasmJsMain/kotlin/dev/ed3c/autowebview/main.kt
@@ -1,8 +1,10 @@
 package dev.ed3c.autowebview
 
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
-import org.jetbrains.compose.ui.window.ComposeViewport
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) { App() }
 }
